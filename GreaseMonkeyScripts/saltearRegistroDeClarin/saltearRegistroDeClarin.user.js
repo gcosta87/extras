8b4e6dbfb85a38f1eaab181ab025fc593747ee29
gcosta87 <gonzalogcosta@yahoo.com.ar>
2015-02-08 18:01:58 -0300

    Cambio menor en header del script. Logo agregado.

--- GreaseMonkeyScripts/saltearRegistroDeClarin/saltearRegistroDeClarin.user.js
+++ GreaseMonkeyScripts/saltearRegistroDeClarin/saltearRegistroDeClarin.user.js
@@ -1,9 +1,11 @@
 // ==UserScript==
-// @name        saltearRegistroDeClarin (Gigya)
+// @name        saltearRegistroDeClarin (Gigya) [versión alpha]
 // @namespace   http://www.libreware.com.ar
 // @description Evita la aparición de la venta modal que solicita el registro para la lectura de los articulos (provisto por el SaaS Gigya)
 // @include     *.clarin.com/*
-// @version     0.1 (alpha)
+// @version     0.1
+// @downloadURL	https://github.com/gcosta87/extras/raw/master/GreaseMonkeyScripts/saltearRegistroDeClarin/saltearRegistroDeClarin.user.js
+// @icon		https://github.com/gcosta87/extras/raw/master/GreaseMonkeyScripts/saltearRegistroDeClarin/logo.png
 // @grant       none
 // ==/UserScript==
 
