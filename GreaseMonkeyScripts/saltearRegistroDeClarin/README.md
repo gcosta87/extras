f71a4ee3e3f4dc16c63cfc9004b126d48421e604
gcosta87 <gonzalogcosta@yahoo.com.ar>
2015-02-08 17:40:00 -0300

    Subida inicial de los GMS. saltearRegistroDeClarin en estado borrador.

---
+++ GreaseMonkeyScripts/saltearRegistroDeClarin/README.md
@@ -0,0 +1,1 @@
+# saltearRegistroDeClarin (Gigya)
---
+++ GreaseMonkeyScripts/saltearRegistroDeClarin/saltearRegistroDeClarin.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        saltearRegistroDeClarin (Gigya)
+// @namespace   http://www.libreware.com.ar
+// @description Evita la aparición de la venta modal que solicita el registro para la lectura de los articulos (provisto por el SaaS Gigya)
+// @include     *.clarin.com/*
+// @version     0.1 (alpha)
+// @grant       none
+// ==/UserScript==
+
+/*Actualmente esta es una prueba que funciona, se estará reemplazando por una solucion mas apropiada*/
+paseE2E.isLoggedIn=function(){return true}
+//paseE2E.closeModal()
